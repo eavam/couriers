197310c822d6f6f630b47bd15a363799b60700f2
Egor Avakumov <avakymov@yandex.ru>
2018-05-26 23:01:17 +0700

    Добавлены тесты автокомплита страны

--- e2e/couriers/filter.js
+++ e2e/couriers/filter.js
@@ -1,16 +1,48 @@
 import { ReactSelector } from 'testcafe-react-selectors'; // first import testcafe selectors
 
-fixture`Getting Started`.page`http://localhost:3000/couriers`; // declare the fixture
+fixture`Страница "Курьеры"`.page`http://localhost:3000/couriers`; // declare the fixture
 
-test('My first test', async t => {
-  const selectCountry = ReactSelector('CustomSelect').withProps(
+test('Автокомплит страны', async t => {
+  const selectCountry = ReactSelector('Select').withProps(
     'data-test',
     'country',
   );
+  const selectOption = ReactSelector('MenuItem').withProps('children', 'Китай');
+
+  const selectResult = selectCountry.find(
+    '.ant-select-selection-selected-value',
+  );
 
   await t
-    .typeText(selectCountry.find('input'), 'Китай')
-    .pressKey('enter')
-    .expect(selectCountry)
-    .eql('Thank you, John Smith!');
+    .click(selectCountry)
+    .typeText(selectCountry, 'Китай', { speed: 0.1 })
+    .click(selectOption)
+    .expect(selectResult.exists)
+    .ok()
+    .expect(selectResult.innerText)
+    .eql('Китай')
+    .expect(selectResult.count)
+    .eql(1);
+});
+
+test('Селект страны', async t => {
+  const selectCountry = ReactSelector('Select').withProps(
+    'data-test',
+    'country',
+  );
+  const selectOption = ReactSelector('MenuItem').withProps('children', 'Китай');
+
+  const selectResult = selectCountry.find(
+    '.ant-select-selection-selected-value',
+  );
+
+  await t
+    .click(selectCountry)
+    .click(selectOption)
+    .expect(selectResult.exists)
+    .ok()
+    .expect(selectResult.innerText)
+    .eql('Китай')
+    .expect(selectResult.count)
+    .eql(1);
 });
